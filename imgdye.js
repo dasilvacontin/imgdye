14cd65b250348f0de5c1dfdd1722c0492c51690a
David da Silva <dasilvacontin@gmail.com>
2015-08-19 20:00:04 +0200

    Fix style and rename main file

---
+++ imgdye.js
@@ -0,0 +1,17 @@
+function imgdye (image, color, opacity) {
+  var buffer = document.createElement('canvas')
+  buffer.width = image.width
+  buffer.height = image.height
+  var ctx = buffer.getContext('2d')
+  if (color) ctx.fillStyle = color
+  if (opacity) ctx.globalAlpha = opacity
+  ctx.fillRect(0, 0, buffer.width, buffer.height)
+  ctx.globalCompositeOperation = 'destination-atop'
+  ctx.globalAlpha = 1
+  ctx.drawImage(image, 0, 0)
+  return buffer
+}
+
+if (typeof module === 'object') {
+  module.exports = imgdye
+}
