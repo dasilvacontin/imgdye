3675500965335d81c26c4b7c33320e8106d239b2
David da Silva <dasilvacontin@gmail.com>
2017-06-01 23:04:37 +0200

    Throw error when `color` is not specified

--- imgdye.js
+++ imgdye.js
@@ -1,14 +1,20 @@
 function imgdye (image, color, opacity) {
+  if (color == null) {
+    throw new Error('Expecting second argument to be a color.')
+  }
+
   var buffer = document.createElement('canvas')
   buffer.width = image.width
   buffer.height = image.height
   var ctx = buffer.getContext('2d')
-  if (color) ctx.fillStyle = color
+
+  ctx.fillStyle = color
   if (opacity != null) ctx.globalAlpha = opacity
   ctx.fillRect(0, 0, buffer.width, buffer.height)
   ctx.globalCompositeOperation = 'destination-atop'
   ctx.globalAlpha = 1
   ctx.drawImage(image, 0, 0)
+
   return buffer
 }
 
